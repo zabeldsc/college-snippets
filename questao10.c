fcff3bde9167adca9157f2abaad6a77a3d72f499
Isabel Costa <125329797+zabeldsc@users.noreply.github.com>
2024-05-17 18:07:46 -0300

    Update questao10.c
    funciona com números negativos

--- questao10.c
+++ questao10.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 
 int main(){
-    // anterior -1 considerando que so serao inseridos numeros de 0 para cima
-    int n, numero, segcresc = 0, anterior = -1, segcrescant = 0;
+    int n, numero, segcresc = 1, anterior = -1, segcrescant = 0;
     
     printf("Quantos numeros voce deseja inserir?\n");
     scanf("%d", &n);
     
-    // while que roda a quantidade de vezes que o usuario deseja inserir numeros
-    while(n > 0){
+    if (n == 1){
+        printf("Seguimento de crescimento igual a %d", segcresc);
+    } else if (n > 0){
+        scanf("%d", &anterior);
+        while (n > 1){
         scanf("%d", &numero);
         // checar se numero eh maior que o numero anterior
-        if (numero > anterior){
+            if (numero > anterior){
             // seguimento crescente aumenta
-            segcresc++;
-            anterior = numero;
-        } else {
-            if (segcresc > segcrescant){
-                printf("ENTROU ELSE!\n");
-                segcrescant = segcresc;
-                segcresc = 0;
-                anterior = -1;
+                segcresc++;
+                anterior = numero;
+            } else {
+                if (segcresc > segcrescant){
+                    printf("ENTROU ELSE!\n");
+                    segcrescant = segcresc;
+                    segcresc = 0;
+                    anterior = -1;
+                }
             }
-        }
         n--;
+        }
     }
-    
+
     if (segcresc < segcrescant){
     	segcresc = segcrescant;
     }
